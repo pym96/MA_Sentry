2713c384c350be56609275f75443974887a4e394
pym96 <pym66@outlook.com>
2023-11-09 19:35:28 +0800

    Add MA sentry urdf

--- ma_sentry_bringup/config/demo_3d.lua
+++ ma_sentry_bringup/config/demo_3d.lua
@@ -1,17 +1,3 @@
--- Copyright 2016 The Cartographer Authors
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---      http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
-
 include "map_builder.lua"
 include "trajectory_builder.lua"
 
@@ -19,7 +5,7 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "camera_init",
+  tracking_frame = "body",
   published_frame = "body",
   odom_frame = "body",
   provide_odom_frame = true,
