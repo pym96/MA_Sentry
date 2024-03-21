3348c41331ff29dfb2252b0e306c4e70d6726a7d
YiMIng <105438207+pym96@users.noreply.github.com>
2024-03-21 16:48:46 +0800

    Update random_explore.hpp

--- rm_control/rm_decision/include/rm_decision/random_explore.hpp
+++ rm_control/rm_decision/include/rm_decision/random_explore.hpp
@@ -5,6 +5,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PointStamped.h>
 #include <std_msgs/Int8.h>
+#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Point.h> // Include for Point data structure
 
 #include <string>
@@ -26,11 +27,18 @@ private:
     ros::NodeHandle nh_;
     ros::Publisher way_point_pub_;
     ros::Subscriber stop_sub_;
+    ros::Subscriber odom_sub_;
     bool goal_reached_;
+
     void stop_callback(const std_msgs::Int8::ConstPtr& msg);
+    void odom_callback(const nav_msgs::Odometry::ConstPtr& msg);
 
     bool initial_position_set; // Flag to check if the initial position has been set
     geometry_msgs::Point initial_position; // Variable to store the initial position
+    
+    double curr_x = 0.0;
+    double curr_y = 0.0;
+
 };
 
 #endif // __RANDOM_EXPLORE_HPP
