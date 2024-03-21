3dbbcf7186445056b19d2afc6c37b3fb7bfed147
YiMIng <105438207+pym96@users.noreply.github.com>
2024-03-21 16:31:21 +0800

    Update random_explore.hpp

--- rm_control/rm_decision/include/rm_decision/random_explore.hpp
+++ rm_control/rm_decision/include/rm_decision/random_explore.hpp
@@ -5,28 +5,32 @@
 #include <ros/ros.h>
 #include <geometry_msgs/PointStamped.h>
 #include <std_msgs/Int8.h>
+#include <geometry_msgs/Point.h> // Include for Point data structure
 
 #include <string>
 
 class RandomExplore: public BT::SyncActionNode
-{   
-    public:
-        RandomExplore(const std::string& name, const BT::NodeConfiguration& config);
+{
+public:
+    RandomExplore(const std::string& name, const BT::NodeConfiguration& config);
 
-        static BT::PortsList providedPorts(){
-            return {};
-        }
+    static BT::PortsList providedPorts(){
+        return {};
+    }
 
-        BT::NodeStatus tick() override;
+    BT::NodeStatus tick() override;
 
-        // void halt() override;
+    // void halt() override;
 
-    private:
-        ros::NodeHandle nh_;
-        ros::Publisher way_point_pub_;
-        ros::Subscriber stop_sub_;
-        bool goal_reached_;
-        void stop_callback(const std_msgs::Int8::ConstPtr& msg);
+private:
+    ros::NodeHandle nh_;
+    ros::Publisher way_point_pub_;
+    ros::Subscriber stop_sub_;
+    bool goal_reached_;
+    void stop_callback(const std_msgs::Int8::ConstPtr& msg);
+
+    bool initial_position_set; // Flag to check if the initial position has been set
+    geometry_msgs::Point initial_position; // Variable to store the initial position
 };
 
-#endif
+#endif // __RANDOM_EXPLORE_HPP
