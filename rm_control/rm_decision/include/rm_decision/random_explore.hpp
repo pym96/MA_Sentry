62553725bd738f3baff4e9613c0612a52923c254
YiMIng <105438207+pym96@users.noreply.github.com>
2024-03-21 16:54:26 +0800

    Update random_explore.hpp

--- rm_control/rm_decision/include/rm_decision/random_explore.hpp
+++ rm_control/rm_decision/include/rm_decision/random_explore.hpp
@@ -6,39 +6,32 @@
 #include <geometry_msgs/PointStamped.h>
 #include <std_msgs/Int8.h>
 #include <nav_msgs/Odometry.h>
-#include <geometry_msgs/Point.h> // Include for Point data structure
+#include <geometry_msgs/Point.h> // 包含Point数据结构的头文件
 
 #include <string>
 
-class RandomExplore: public BT::SyncActionNode
-{
+class RandomExplore : public BT::SyncActionNode {
 public:
     RandomExplore(const std::string& name, const BT::NodeConfiguration& config);
 
-    static BT::PortsList providedPorts(){
+    static BT::PortsList providedPorts() {
         return {};
     }
 
     BT::NodeStatus tick() override;
 
-    // void halt() override;
-
 private:
     ros::NodeHandle nh_;
     ros::Publisher way_point_pub_;
-    ros::Subscriber stop_sub_;
-    ros::Subscriber odom_sub_;
-    bool goal_reached_;
+    ros::Subscriber stop_sub_;    // 订阅停止信号
+    ros::Subscriber odom_sub_;    // 订阅里程计信息
 
-    void stop_callback(const std_msgs::Int8::ConstPtr& msg);
-    void odom_callback(const nav_msgs::Odometry::ConstPtr& msg);
+    bool goal_reached_;  // 目标是否已达到的标志
+    bool initial_position_set;  // 初始位置是否已设置的标志
+    geometry_msgs::Point initial_position;  // 存储初始位置
 
-    bool initial_position_set; // Flag to check if the initial position has been set
-    geometry_msgs::Point initial_position; // Variable to store the initial position
-    
-    double curr_x = 0.0;
-    double curr_y = 0.0;
-
+    void stop_callback(const std_msgs::Int8::ConstPtr& msg);  // 处理停止信号的回调函数
+    void odom_callback(const nav_msgs::Odometry::ConstPtr& msg);  // 处理里程计信息的回调函数
 };
 
 #endif // __RANDOM_EXPLORE_HPP
