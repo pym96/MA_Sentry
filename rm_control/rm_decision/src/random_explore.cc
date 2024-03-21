b534149e036ffb5fbd69cdf3237f17c3607939e7
YiMIng <105438207+pym96@users.noreply.github.com>
2024-03-21 16:49:04 +0800

    Update random_explore.cc

--- rm_control/rm_decision/src/random_explore.cc
+++ rm_control/rm_decision/src/random_explore.cc
@@ -9,8 +9,14 @@ RandomExplore::RandomExplore(const std::string& name, const BT::NodeConfiguratio
     nh_ = ros::NodeHandle("~");
     way_point_pub_ = nh_.advertise<geometry_msgs::PointStamped>("/way_point", 10);
     stop_sub_ = nh_.subscribe("/stop", 10, &RandomExplore::stop_callback, this);
+    odom_sub_ = nh_.subscribe<nav_msgs::Odometry>("/state_estimation", 5, &RandomExplore::odom_callback);
     goal_reached_ = false;
     srand(time(NULL));
+}
+
+void RandomExplore::odom_callback(const nav_msgs::Odometry::ConstPtr& msg){
+    this->curr_x = msg->pose.pose.position.x;
+    this->curr_y = msg->pose.pose.position.y;
 }
 
 void RandomExplore::stop_callback(const std_msgs::Int8::ConstPtr& msg){
@@ -25,8 +31,8 @@ BT::NodeStatus RandomExplore::tick(){
     if (!initial_position_set) {
         // Assume current position is (0, 0) for the example, 
         // but you should retrieve the actual robot position here.
-        initial_position.x = 0;
-        initial_position.y = 0;
+        initial_position.x = curr_x;
+        initial_position.y = curr_y;
         initial_position_set = true;
     }
 
@@ -34,7 +40,7 @@ BT::NodeStatus RandomExplore::tick(){
     float angle = static_cast<float>(rand()) / RAND_MAX * 2 * M_PI;
     float radius = static_cast<float>(rand()) / RAND_MAX * 5;   
     geometry_msgs::PointStamped new_goal;
-    new_goal.header.frame_id = "map"; // Assuming the fixed frame is "map"
+    new_goal.header.frame_id = "vehicle"; // Assuming the fixed frame is "map"
     new_goal.point.x = initial_position.x + cos(angle) * radius;
     new_goal.point.y = initial_position.y + sin(angle) * radius;
     new_goal.header.stamp = ros::Time::now();
