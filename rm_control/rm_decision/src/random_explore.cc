c1c1c5261a4c4c8c274737f250f1086884b481ed
YiMIng <105438207+pym96@users.noreply.github.com>
2024-03-21 16:31:39 +0800

    Update random_explore.cc

--- rm_control/rm_decision/src/random_explore.cc
+++ rm_control/rm_decision/src/random_explore.cc
@@ -4,7 +4,7 @@
 #include <time.h>
 
 RandomExplore::RandomExplore(const std::string& name, const BT::NodeConfiguration& config)
-    : BT::SyncActionNode(name, config){
+    : BT::SyncActionNode(name, config), initial_position_set(false) {
 
     nh_ = ros::NodeHandle("~");
     way_point_pub_ = nh_.advertise<geometry_msgs::PointStamped>("/way_point", 10);
@@ -22,19 +22,23 @@ BT::NodeStatus RandomExplore::tick(){
         return BT::NodeStatus::SUCCESS;
     }
 
-    // Generate a random point within a 5-meter radius
+    if (!initial_position_set) {
+        // Assume current position is (0, 0) for the example, 
+        // but you should retrieve the actual robot position here.
+        initial_position.x = 0;
+        initial_position.y = 0;
+        initial_position_set = true;
+    }
+
+    // Generate a random point within a 5-meter radius from the initial position
     float angle = static_cast<float>(rand()) / RAND_MAX * 2 * M_PI;
-    float radius = static_cast<float>(rand()) / RAND_MAX * 0.5;   
+    float radius = static_cast<float>(rand()) / RAND_MAX * 5;   
     geometry_msgs::PointStamped new_goal;
-    new_goal.header.frame_id = "map";
-    new_goal.point.x = cos(angle) * radius;
-    new_goal.point.y = sin(angle) * radius;
+    new_goal.header.frame_id = "map"; // Assuming the fixed frame is "map"
+    new_goal.point.x = initial_position.x + cos(angle) * radius;
+    new_goal.point.y = initial_position.y + sin(angle) * radius;
     new_goal.header.stamp = ros::Time::now();
     way_point_pub_.publish(new_goal);
 
     return BT::NodeStatus::FAILURE;
 }
-
-// void RandomExplore::halt(){
-//     // Do something;
-// }
