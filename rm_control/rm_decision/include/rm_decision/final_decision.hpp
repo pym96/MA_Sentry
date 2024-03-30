55e9379caa4980892e5deea6f0ee8656da0e9170
YiMIng <105438207+pym96@users.noreply.github.com>
2024-03-30 10:43:58 +0800

    Update final_decision.hpp

--- rm_control/rm_decision/include/rm_decision/final_decision.hpp
+++ rm_control/rm_decision/include/rm_decision/final_decision.hpp
@@ -7,6 +7,7 @@
 #include <geometry_msgs/Point.h>
 #include <geometry_msgs/PointStamped.h>
 #include <std_msgs/Int32.h>
+#include <std_msgs/UInt16.h>
 
 class MoveToPosition : public BT::SyncActionNode
 {
@@ -57,21 +58,38 @@ private:
 class CheckHP : public BT::ConditionNode
 {
 public:
-        CheckHP(const std::string& name, const BT::NodeConfiguration& config)
-            : BT::ConditionNode(name, config) {}
+    CheckHP(const std::string& name, const BT::NodeConfiguration& config)
+        : BT::ConditionNode(name, config), global_current_hp(600) // 初始化血量
+    {
+        // 在构造函数中订阅，确保只订阅一次
+        hp_sub_ = nh_.subscribe("/sentry_hp", 5, &CheckHP::hp_callback, this);
+    }
 
-        BT::NodeStatus tick() override {
-            // 使用全局或通过某种机制传递的血量变量
-            return global_current_hp < 400 ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    BT::NodeStatus tick() override {
+        uint16_t current_hp_copy;
+        {
+            std::lock_guard<std::mutex> lock(hp_mutex_);
+            current_hp_copy = global_current_hp;
         }
+        return current_hp_copy < 400 ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    }
 
-        static BT::PortsList providedPorts() {
-            return {};
-        }
-    private:
-        int global_current_hp = 1000;
+    static BT::PortsList providedPorts() {
+        return {};
+    }
 
+private:
+    uint16_t global_current_hp;
+    ros::NodeHandle nh_;
+    ros::Subscriber hp_sub_;
+    std::mutex hp_mutex_; // 用于保护 global_current_hp 的互斥锁
+
+    void hp_callback(const std_msgs::UInt16::ConstPtr& msg) {
+        std::lock_guard<std::mutex> lock(hp_mutex_);
+        global_current_hp = msg->data;
+    }
 };
+
 
 class Wait : public BT::SyncActionNode { public: Wait(const std::string& name, const BT::NodeConfiguration& config) : BT::SyncActionNode(name, config) {}
 
